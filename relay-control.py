77abdcd26bd139401495e0210c8e44cce7a35872
Vojin Jovanovic <vojin.jovanovic@oracle.com>
2017-09-13 00:31:36 +0200

    startup scripts

--- relay-control.py
+++ relay-control.py
@@ -10,8 +10,6 @@ except RuntimeError:
 from time import sleep
 import os
 
-GPIO.setmode(GPIO.BCM)
-
 RELAY_GPIO_MAP = {
     0: 5,
     1: 6,
@@ -23,12 +21,14 @@ RELAY_GPIO_MAP = {
     7: 20,
 }
 
-for pin in RELAY_GPIO_MAP.values():
-    GPIO.setup(pin, GPIO.OUT)
+
+def setup_GPIO():
+    GPIO.setmode(GPIO.BCM)
+    for pin in RELAY_GPIO_MAP.values():
+        GPIO.setup(pin, GPIO.OUT)
 
 
 def main():
-    switches = [0 for _ in iter(range(len(RELAY_GPIO_MAP)))]
     if len(sys.argv) != 3:
         sys.stderr.write(
             "relay-control.py expects two arguments pointing to the PID file and to the folder with relay states.\n")
@@ -36,45 +36,52 @@ def main():
 
     pid_file = sys.argv[1]
     if not os.path.isfile(pid_file):
-        sys.stderr.write(
-            "PID file does not exist: " + pid_file)
+        sys.stderr.write("PID file does not exist: " + pid_file)
         sys.exit(1)
 
-    with open(pid_file, "w") as text_file:
-        text_file.write(str(os.getpid()))
+    try:
+        setup_GPIO()
+        switches = [0 for _ in iter(range(len(RELAY_GPIO_MAP)))]
+        with open(pid_file, "w") as text_file:
+            text_file.write(str(os.getpid()))
 
-    path = sys.argv[2]
-    time = 0
-    while True:
-        for relay in iter(range(len(RELAY_GPIO_MAP))):
-            switch_state_file_path = path + os.sep + str(relay)
-            content = ""
-            try:
-                with open(switch_state_file_path, 'r') as content_file:
-                    content = content_file.read().strip()
-                    value = int(content)
-                    if value == 0 or value == 1:
-                        switches[relay] = value
-                    else:
-                        sys.stderr.write("For switch " + str(
-                            relay) + " expected 0 or 1 but found " + str(value) + ". Keeping the previous state.\n")
-            except IOError:
-                sys.stderr.write("Can't open the switch file " + switch_state_file_path + ".\n")
-            except ValueError:
-                sys.stderr.write("Can't read state from the switch " + str(
-                    relay) + ": expected a number 0 or 1. Found " + content + ". Keeping the previous state.\n")
+        path = sys.argv[2]
+        time = 0
+        while True:
+            for relay in iter(range(len(RELAY_GPIO_MAP))):
+                switch_state_file_path = path + os.sep + str(relay)
+                content = ""
+                try:
+                    with open(switch_state_file_path, 'r') as content_file:
+                        content = content_file.read().strip()
+                        value = int(content)
+                        if value == 0 or value == 1:
+                            switches[relay] = value
+                        else:
+                            sys.stderr.write("For switch " + str(
+                                relay) + " expected 0 or 1 but found " + str(value) + ". Keeping the previous state.\n")
+                except IOError:
+                    sys.stderr.write("Can't open the switch file " + switch_state_file_path + ".\n")
+                except ValueError:
+                    sys.stderr.write("Can't read state from the switch " + str(
+                        relay) + ": expected a number 0 or 1. Found " + content + ". Keeping the previous state.\n")
 
-            # setting the relay state according to switch states
-            assert switches[relay] == 0 or switches[relay] == 1
+                # setting the relay state according to switch states
+                assert switches[relay] == 0 or switches[relay] == 1
 
-            # make sure the pump is running if the heating is on
-            if (relay == 1 or relay == 2) and switches[relay] == 1:
-                assert switches[0] == 1
+                # make sure the pump is running if the heating is on
+                if (relay == 1 or relay == 2) and switches[relay] == 1:
+                    assert switches[0] == 1
 
-            # GPIO.output(RELAY_GPIO_MAP[relay], switches[relay])
+                GPIO.output(RELAY_GPIO_MAP[relay], switches[relay])
 
-        sleep(POLL_PERIOD_SECONDS)
-        time += 1
+            sleep(POLL_PERIOD_SECONDS)
+            time += 1
+
+    finally:
+        GPIO.cleanup()
+        with open(pid_file, "w") as text_file:
+            text_file.write("99999999")
 
 
 if __name__ == '__main__':
