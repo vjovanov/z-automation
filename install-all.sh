59c85e88a54aac4c6c84411598627dca88c9eb06
Vojin Jovanovic <vojin.jovanovic@oracle.com>
2018-10-29 22:26:43 +0100

    Sensor fixes

--- install-all.sh
+++ install-all.sh
@@ -3,19 +3,26 @@
 PI_HOME="/home/pi/"
 CODE_HOME="$PI_HOME/z-automation/"
 
+mkdir $PI_HOME/heating
+echo 0 > $PI_HOME/heating/electrical-heating-switch
+echo 0 > $PI_HOME/heating/gas-heating-switch
 
+hassbian-config install razberry
+apt install nmap
+apt install libudev-dev
+apt install vim
+
+# z-wave
+wget -q -O - razberry.z-wave.me/install 14 | sudo bash
+sudo update-rc.d z-way-server remove
+
+# GPIO
+apt-get install python-rpi.gpio python3-rpi.gpio
+
+# Relays
+mkdir -p /var/run
 cp "$PI_HOME/z-automation/relay-control.sh" /etc/init.d/relay-control-daemon
 chmod +x /etc/init.d/relay-control-daemon
 update-rc.d relay-control-daemon defaults
 service relay-control-daemon install
 
-cp "$PI_HOME/z-automation/wifi-reseter.sh" /etc/init.d/wifi-reseter-daemon
-chmod +x /etc/init.d/wifi-reseter-daemon
-update-rc.d wifi-reseter-daemon defaults
-service wifi-reseter-daemon install
-
-
-cp "$PI_HOME/z-automation/hass-daemon.sh"  /etc/init.d/hass-daemon
-chmod +x /etc/init.d/hass-daemon
-update-rc.d hass-daemon defaults
-service hass-daemon install
